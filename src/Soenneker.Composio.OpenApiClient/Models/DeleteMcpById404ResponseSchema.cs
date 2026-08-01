1aadd7311c9d05ab70c1839378aaa48409107753
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-08-01 17:48:11 +0000

    Automated update

--- src/Soenneker.Composio.OpenApiClient/Models/DeleteMcpById404ResponseSchema.cs
+++ src/Soenneker.Composio.OpenApiClient/Models/DeleteMcpById404ResponseSchema.cs
@@ -10,7 +10,7 @@ namespace Soenneker.Composio.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class DeleteMcpById404ResponseResponseJson : ApiException, IAdditionalDataHolder, IParsable
+    public partial class DeleteMcpById404ResponseSchema : ApiException, IAdditionalDataHolder, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
@@ -18,21 +18,21 @@ namespace Soenneker.Composio.OpenApiClient.Models
         /// <summary>The primary error message.</summary>
         public override string Message { get => base.Message; }
         /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.Composio.OpenApiClient.Models.DeleteMcpById404ResponseResponseJson"/> and sets the default values.
+        /// Instantiates a new <see cref="global::Soenneker.Composio.OpenApiClient.Models.DeleteMcpById404ResponseSchema"/> and sets the default values.
         /// </summary>
-        public DeleteMcpById404ResponseResponseJson()
+        public DeleteMcpById404ResponseSchema()
         {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Composio.OpenApiClient.Models.DeleteMcpById404ResponseResponseJson"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Composio.OpenApiClient.Models.DeleteMcpById404ResponseSchema"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.Composio.OpenApiClient.Models.DeleteMcpById404ResponseResponseJson CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.Composio.OpenApiClient.Models.DeleteMcpById404ResponseSchema CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Composio.OpenApiClient.Models.DeleteMcpById404ResponseResponseJson();
+            return new global::Soenneker.Composio.OpenApiClient.Models.DeleteMcpById404ResponseSchema();
         }
         /// <summary>
         /// The deserialization information for the current model
